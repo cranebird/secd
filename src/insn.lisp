ca4037855e53ba839bf1f4b03ed78e1ea46146c3
cranebird <quasi@kc4.so-net.ne.jp>
2010-05-05 10:51:14 +0900

    refactoring. - rename scheme-value-of to value-of. - rename convert-to-scheme-value to convert-scheme-obj. etc.

--- src/insn.lisp
+++ src/insn.lisp
@@ -11,21 +11,31 @@
 ;; VM instructions
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
-(in-package :secd)
+(in-package :secd.vm)
 
 (defmacro vm-assert (&rest rest)
   (when *debug*
       `(assert ,@rest)))
 
+(defvar *the-vm* nil)
+
+(defmacro cons (a b)
+  `(vm-cons *the-vm* ,a ,b))
+
+(defmacro car (a)
+  `(vm-car *the-vm* ,a))
+
+(defmacro cdr (a)
+  `(vm-cdr *the-vm* ,a))
+
 (defmacro def-insn (name (vm) &rest body)
   "define instuction."
   (let ((insn (gensym)))
     `(defmethod dispatch ((,insn (eql ,(as-keyword name))) (,vm vm))
+       ;; (declare (optimize (speed 3) (safety 0)))
        ;; (when (> (execution-count-of ,vm) 1)
        ;;   (setf (ap-of ,vm) (copying ,vm))
        ;;   (swap-space ,vm))
-
        ,(if *debug*
             `(let ((*print-circle* t))
                (format t "; PC: ~a insn: ~a~%" (pc-of ,vm) ,insn)
@@ -37,31 +47,26 @@
                (incf (execution-count-of ,vm))
                (incf (gethash ,(as-keyword name) (profile-of ,vm) 0))))
        ;;
-       
-       ;;
-       ,@body
-       )))
+       (let ((*the-vm* ,vm))
+         ,@body))))
+
+;; STOP
+(def-insn STOP (vm)
+  (signal 'stop-vm-condition))
 
 ;; NIL     s e (NIL.c) d        ->  (nil.s) e c d
 (def-insn NIL (vm)
   (with-accessors ((sp sp-of)) vm
     (vm-assert (eql (scheme-type-of sp) :pair) (sp) "NIL: scheme-type-of sp should be :PAIR")
-    (setf sp (vm-cons vm empty sp)) ;; cons
-    (vm-assert (eql (scheme-type-of sp) :pair) (sp) "NIL: scheme-type-of sp should be :PAIR")
-    (next vm)))
+    (setf sp (cons empty sp))
+    (vm-assert (eql (scheme-type-of sp) :pair) (sp) "NIL: scheme-type-of sp should be :PAIR")))
 
-;; STOP
-(def-insn STOP (vm)
-  vm)
-
-;; LDC
+;; LDC     s e (LDC x.c) d      ->  (x.s) e c d
 (def-insn LDC (vm)
   (with-accessors ((pc pc-of) (sp sp-of)) vm
     (let ((c (fetch-operand vm)))
-      ;(assert (integerp c))
-      (setf sp (vm-cons vm (immediate-rep c) sp))
-      (incf pc)
-      (next vm))))
+      (setf sp (cons (immediate-rep c) sp))
+      (incf pc))))
 
 (defmacro def-binary-insn (name sym)
   (let ((a (gensym))
@@ -71,135 +76,100 @@
     `(progn
        (def-insn ,name (,vm)
          (with-accessors ((,sp sp-of)) ,vm
-           (let* ((,a (vm-car ,vm ,sp))
-                  (,b (vm-car ,vm (vm-cdr ,vm ,sp))))
+           (let* ((,a (car ,sp))
+                  (,b (car (cdr ,sp))))
              ;; todo; not fast
              (setf ,sp
-                   (vm-cons ,vm
-                            (immediate-rep
-                             (,sym (scheme-value-of ,a) (scheme-value-of ,b))) ,sp)))
-           (next ,vm))))))
+                   (cons
+                    (immediate-rep
+                     (,sym (value-of ,a) (value-of ,b))) ,sp))))))))
 
+;; +       (a b.s) e (OP.c) d   ->   ((a OP b).s) e c d
 ;; x = 4a; y = 4b; (x + y) = (4a + 4b) = 4(a + b)
 (def-insn + (vm)
   (with-accessors ((sp sp-of)) vm
-    (let* ((a (vm-car vm sp))
-           (b (vm-car vm (vm-cdr vm sp)))
+    (let* ((a (car sp))
+           (b (car (cdr sp)))
            (res (+ a b)))
-      (setf sp (vm-cons vm res (vm-cdr vm (vm-cdr vm sp))))
-      (next vm))))
+      (setf sp (cons res (cdr (cdr sp)))))))
 
 ;; x = 4a; y = 4b; (x - y) = (4a - 4b) = 4(a - b)
 (def-insn - (vm)
   (with-accessors ((sp sp-of)) vm
-    (let* ((a (vm-car vm sp))
-           (b (vm-car vm (vm-cdr vm sp)))
+    (let* ((a (car sp))
+           (b (car (cdr sp)))
            (res (- a b)))
-      (setf sp (vm-cons vm res (vm-cdr vm (vm-cdr vm sp))))
-      (next vm))))
+      (setf sp (cons res (cdr (cdr sp)))))))
 
 (def-binary-insn * cl:*)
 
 ;; x == y then 4x == 4y
 (def-insn = (vm)
   (with-accessors ((sp sp-of)) vm
-    (let* ((a (vm-car vm sp))
-           (b (vm-car vm (vm-cdr vm sp)))
+    (let* ((a (car sp))
+           (b (car (cdr sp)))
            (res (if (= a b) bool-t bool-f)))
-      (setf sp (vm-cons vm res (vm-cdr vm (vm-cdr vm sp))))
-      (next vm))))
+      (setf sp (cons res (cdr (cdr sp)))))))
 
 (def-insn > (vm)
   (with-accessors ((sp sp-of)) vm
-    (let* ((a (vm-car vm sp))
-           (b (vm-car vm (vm-cdr vm sp)))
-           (res (if (> (scheme-value-of a) (scheme-value-of b))
+    (let* ((a (car sp))
+           (b (car (cdr sp)))
+           (res (if (> (value-of a) (value-of b))
                     bool-t
                     bool-f)))
-      (setf sp (vm-cons vm res (vm-cdr vm (vm-cdr vm sp))))
-      (next vm))))
+      (setf sp (cons res (cdr (cdr sp)))))))
 
 (def-insn < (vm)
   (with-accessors ((sp sp-of)) vm
-    (let* ((a (vm-car vm sp))
-           (b (vm-car vm (vm-cdr vm sp)))
-           (res (if (< (scheme-value-of a) (scheme-value-of b))
+    (let* ((a (car sp))
+           (b (car (cdr sp)))
+           (res (if (< (value-of a) (value-of b))
                     bool-t
                     bool-f)))
-      (setf sp (vm-cons vm res (vm-cdr vm (vm-cdr vm sp))))
-      (next vm))))
+      (setf sp (cons res (cdr (cdr sp)))))))
 
 ;; CONS
 (def-insn CONS (vm)
   (with-accessors ((sp sp-of)) vm
-    (let* ((a (vm-car vm sp))
-           (b (vm-car vm (vm-cdr vm sp))))
-      (setf sp (vm-cons vm (vm-cons vm a b) (vm-cdr vm (vm-cdr vm sp))))
-      (next vm))))
+    (let* ((a (car sp))
+           (b (car (cdr sp))))
+      (setf sp (cons (cons a b) (cdr (cdr sp)))))))
 
 (def-insn CAR (vm)
   (with-accessors ((sp sp-of)) vm
-    (setf sp (vm-car vm sp))
-    (next vm)))
+    (setf sp (car sp))))
 
 (def-insn CDR (vm)
   (with-accessors ((sp sp-of)) vm
     (vm-assert (eql (scheme-type-of sp) :pair) (sp) "CDR: scheme-type-of sp should be :PAIR")
-    (let* ((a (vm-car vm sp))
-           (x (vm-cdr vm a)))
+    (let* ((a (car sp))
+           (x (cdr a)))
       (vm-assert (eql (scheme-type-of a) :pair) (a) "CDR: scheme-type-of a should be :PAIR")
-      (setf sp (vm-cons vm x (vm-cdr vm sp)))
-      (vm-assert (eql (scheme-type-of sp) :pair) (sp) "CDR: scheme-type-of sp should be :PAIR")
-      (next vm))))
-
-;; (def-insn CDR (vm)
-;;   (with-accessors ((sp sp-of)) vm
-;;     (let* ((a (vm-stack-pop vm)))
-;;       (setf sp (vm-cons vm (vm-cdr vm a) sp))
-;;       (next vm))))
+      (setf sp (cons x (cdr sp)))
+      (vm-assert (eql (scheme-type-of sp) :pair) (sp) "CDR: scheme-type-of sp should be :PAIR"))))
 
 ;; SEL CT CF CONT
 ;; SEL     (x.s) e (SEL ct cf.c) d   ->  s e c' (c.d)
 ;;                 where c' = ct if x is T, and cf if x is F
 (def-insn SEL (vm)
   (with-accessors ((sp sp-of) (pc pc-of) (code get-code) (dump dump-of)) vm
-    (let* ((x (vm-car vm sp))
+    (let* ((x (car sp))
            (ct (code-ref code pc))
            (cf (code-ref code (1+ pc)))
            (cont (code-ref code (+ 2 pc))))
-      (setf sp (vm-cdr vm sp)
-            pc (if (eql (scheme-value-of x) '#t)
+      (setf sp (cdr sp)
+            pc (if (eql (value-of x) '#t)
                    ct
                    cf)
-            dump (vm-cons vm (immediate-rep cont) dump))
-      (next vm))))
+            dump (cons (immediate-rep cont) dump)))))
 
-;; (def-insn SEL (vm)
-;;   (with-accessors ((pc pc-of) (code get-code) (dump dump-of)) vm
-;;     (let* ((x (vm-stack-pop vm))
-;;            (ct (code-ref code pc))
-;;            (cf (code-ref code (1+ pc)))
-;;            (cont (code-ref code (+ 2 pc))))
-;;       (setf pc (if (eql (scheme-value-of x) '#t)
-;;                    ct
-;;                    cf)
-;;             dump (vm-cons vm (immediate-rep cont) dump))
-;;       (next vm))))
-
-;; JOIN
 ;; JOIN    s e (JOIN.c) (cr.d)  ->  s e cr d
 (def-insn JOIN (vm)
   (with-accessors ((dump dump-of) (pc set-pc)) vm
-    (let ((cr (vm-car vm dump)))
-      (setf pc (scheme-value-of cr))
-      (setf dump (vm-cdr vm dump))
-      (next vm))))
-
-;; (def-insn JOIN (vm)
-;;   (with-accessors ((pc set-pc)) vm
-;;     (let ((cr (vm-dump-pop vm)))
-;;       (setf pc (scheme-value-of cr))
-;;       (next vm))))
+    (let ((cr (car dump)))
+      (setf pc (value-of cr))
+      (setf dump (cdr dump)))))
 
 ;;(defun locate (level j env)
 ;;  "Return i th variable in j level in environment ENV in runtime."
@@ -210,12 +180,13 @@
 
 (defmethod locate ((vm vm) level n)
   (with-accessors ((env env-of)) vm
-    (let ((env* (loop :repeat level
-                   :for e = env :then (vm-cdr vm e)
-                   :finally (return (vm-car vm e)))))
-      (loop :repeat n
-         :for var = env* :then (vm-cdr vm var)
-         :finally (return (vm-car vm var))))))
+    (let ((*the-vm* vm))
+      (let ((env* (loop :repeat level
+                     :for e = env :then (cdr e)
+                     :finally (return (car e)))))
+        (loop :repeat n
+           :for var = env* :then (cdr var)
+           :finally (return (car var)))))))
 
 ;; LD      s e (LD (i.j).c) d   ->  (locate((i.j),e).s) e c d
 (def-insn LD (vm)
@@ -223,67 +194,49 @@
     (let ((level (code-ref code pc))
           (n (code-ref code (1+ pc)))
           (oldpc (get-pc vm)))
-      (setf sp (vm-cons vm (locate vm level n) sp)
-            pc (+ 2 oldpc))
-      (next vm))))
+      (setf sp (cons (locate vm level n) sp)
+            pc (+ 2 oldpc)))))
 
 ;; LDF     s e (LDF f.c) d            ->  ((f.e).s) e c d
 (def-insn LDF (vm)
   (with-accessors ((sp sp-of) (env get-env) (pc pc-of) (code get-code)) vm
     (let ((f (fetch-operand vm)) ;; PC
           (c (code-ref code (+ 1 pc))))
-      (setf sp (vm-cons vm (vm-cons vm (immediate-rep f) env) sp)
-            pc c)
-      (next vm))))
+      (setf sp (cons (cons (immediate-rep f) env) sp)
+            pc c))))
 
 ;; AP      ((f.e') v.s) e (AP.c) d    ->  NIL (v.e') f (s e c.d)
 (def-insn AP (vm)
   (with-accessors ((env env-of) (pc pc-of) (dump dump-of) (sp sp-of) (code get-code)) vm
     (let* ((c pc) ;;
-           (closure (vm-car vm sp)) ;; (f.e')
-           (fbody-pc (scheme-value-of (vm-car vm closure))) ;; f
-           (fenv (vm-cdr vm closure)) ;; e'
-           (v (vm-car vm (vm-cdr vm sp))) ;; v
-           (s (vm-cdr vm (vm-cdr vm sp))) ;; s
+           (closure (car sp)) ;; (f.e')
+           (fbody-pc (value-of (car closure))) ;; f
+           (fenv (cdr closure)) ;; e'
+           (v (car (cdr sp))) ;; v
+           (s (cdr (cdr sp))) ;; s
            (oldenv env))
-      (setf sp (vm-cons vm empty empty)
-            env (vm-cons vm v fenv)
+      (setf sp (cons empty empty)
+            env (cons v fenv)
             pc fbody-pc
-            dump (vm-cons vm s (vm-cons vm oldenv (vm-cons vm (immediate-rep c) dump))))
-      (next vm))))
+            dump (cons s (cons oldenv (cons (immediate-rep c) dump)))))))
 
 ;; RTN     (x.z) e' (RTN.q) (s e c.d) ->  (x.s) e c d
 (def-insn RTN (vm)
   (with-accessors ((sp sp-of) (env set-env) (pc pc-of) (dump dump-of)) vm
-    (let* ((x (vm-car vm sp))
-           (s (vm-car vm dump))
-           (e (vm-car vm (vm-cdr vm dump)))
-           (c (vm-car vm (vm-cdr vm (vm-cdr vm dump))))
-           (d (vm-cdr vm (vm-cdr vm (vm-cdr vm dump)))))
-      (setf sp (vm-cons vm x s)
+    (let* ((x (car sp))
+           (s (car dump))
+           (e (car (cdr dump)))
+           (c (car (cdr (cdr dump))))
+           (d (cdr (cdr (cdr dump)))))
+      (setf sp (cons x s)
             env e
-            pc (scheme-value-of c)
-            dump d)
-      (next vm))))
-
-;; (def-insn RTN (vm)
-;;   (with-accessors ((sp sp-of) (env set-env) (pc pc-of) (dump dump-of)) vm
-;;     (let* ((x (vm-stack-pop vm))
-;;            (s (vm-car vm dump))
-;;            (e (vm-car vm (vm-cdr vm dump)))
-;;            (c (vm-car vm (vm-cdr vm (vm-cdr vm dump))))
-;;            (d (vm-cdr vm (vm-cdr vm (vm-cdr vm dump)))))
-;;       (setf sp (vm-cons vm x s)
-;;             env e
-;;             pc (scheme-value-of c)
-;;             dump d)
-;;       (next vm))))
+            pc (value-of c)
+            dump d))))
 
 ;; DUM
 (def-insn DUM (vm)
   (with-accessors ((env env-of)) vm
-    (setf env (vm-cons vm (gensym) env))
-    (next vm)))
+    (setf env (cons (gensym) env))))
 
 ;; RAP     ((f.(W.e)) v.s) (W.e) (RAP.c) d  ->  nil rplaca((W.e),v) f (s e c.d)
 (def-insn RAP (vm)
@@ -301,6 +254,6 @@
 ;;         (setf env WW)
 ;;         (setf pc f)
 ;;         (setf dump (append (list s (cdr WW) c) dump))
-;;         (next vm)))))
+;;         ))))
 
 
