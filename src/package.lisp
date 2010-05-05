e30dde2db458d7f390ea861e71c61ead1f742450
cranebird <quasi@kc4.so-net.ne.jp>
2010-05-05 10:51:07 +0900

    refactoring. - rename scheme-value-of to value-of. - rename convert-to-scheme-value to convert-scheme-obj. etc.

---
+++ src/package.lisp
@@ -0,0 +1,45 @@
+(defpackage :secd.util
+  (:use :common-lisp)
+  (:export :set-scm-macro-character :as-keyword))
+
+(defpackage :secd.compile
+  (:use :common-lisp :secd.util)
+  (:export :extend-env :lookup :compile-exp
+           :compile-pass1
+           :compile-pass2
+           ))
+
+(defpackage :secd.vm
+  (:use :common-lisp :secd.util)
+  (:shadow :cons :car :cdr)
+  (:export :make-vm :describe-object :print-object :vm-car :vm-cdr :vm-cons :*debug*
+           :*profile* :dispatch :vm
+           :vm-stack-top
+           :sp-of :get-sp :set-sp
+           :ap-of :get-ap :set-ap
+           :env-of :get-env :set-env
+           :pc-of :get-pc :set-pc
+           :code-of :get-code :set-code
+           :dump-of :get-dump :set-dump
+           :code-ref
+           :value-of
+           :convert-scheme-obj
+           :immediate-rep
+           :profile-of :get-profile
+           :execution-count-of
+           :bool-f
+           :bool-t
+           :empty
+           :fetch-operand
+           :graphviz-vm
+           :fetch-insn
+           :stop-vm-condition
+           :unknown-immediate-rep-error
+           ))
+
+(defpackage :secd
+  (:use :common-lisp :secd.util :secd.compile :secd.vm :asdf :com.gigamonkeys.test)
+  )
+
+(defpackage :secd.test
+  (:use :common-lisp :secd))
