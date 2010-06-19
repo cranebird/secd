039772f073a1b6a07d020597019ea5807a2917e2
cranebird <quasi@kc4.so-net.ne.jp>
2010-06-19 10:14:21 +0900

    expand match and remove list in def-secd-machine.

--- src/interp.lisp
+++ src/interp.lisp
@@ -10,6 +10,7 @@
       pattern))
 
 (defun collect-syms (lst)
+  "collect not instruction."
   (remove-if #'instruction-p (remove-duplicates (flatten lst))))
 
 (defun ignorable-sym (state1 state2)
@@ -17,7 +18,7 @@
      :if (not (member x (intersection (collect-syms state1) (collect-syms state2))))
      :collect x))
 
-(defun validate-states (rule)
+(defun validate-rule (rule)
   (flet ((check (init trans)
                 (let ((diff (set-difference (collect-syms trans) (collect-syms init))))
                   (unless (null diff)
@@ -32,24 +33,28 @@
        (error "rule is not expected form: ~s" rule)))))
 
 (defun validate-rules (rules)
-  (loop :for rule :in rules :do (validate-states rule))
   (let ((ht (make-hash-table :test #'equal)))
     (loop :for rule :in rules
        :do
+       (validate-rule rule)
        (multiple-value-bind (val present-p)
            (gethash rule ht)
          (if present-p
              (error "multiple rule found: ~s" val)
              (setf (gethash rule ht) rule))))))
 
-;; (defun locate (m n e) ;; for Common Lisp interpreter
-;;   (declare (optimize (speed 3) (safety 0)))
-;;   (declare (fixnum m n ))
-;;   (nth (the fixnum (1- n)) (nth (the fixnum (1- m)) e)))
-
 (defun locate (m n e) ;; for Common Lisp interpreter
-  (nth (1- n) (nth (1- m) e)))
+  (declare (optimize (speed 3) (safety 0)))
+  (declare (fixnum m n ))
+  (nth (the fixnum (1- n)) (nth (the fixnum (1- m)) e)))
 
+;; (defun locate (m n e) ;; for Common Lisp interpreter
+;;   (nth (1- n) (nth (1- m) e)))
+
+(defun make-vector (l)
+  (apply #'vector l))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun max-rule-width (rules)
   (let ((states
          (loop :for rule :in rules
@@ -78,15 +83,13 @@
         (max-rule-width rules)
       (let ((w (+ (apply #'+ (list s0w e0w c0w d0w s1w e1w c1w d1w))
                   (length "-> "))))
-        (flet ((hr ()
-                 (loop :repeat w :do (format out "-") :finally (format out "~%"))))
+        (flet ((hr () (loop :repeat w :do (format out "-") :finally (format out "~%"))))
           (format out
                   "           INITIAL STATE                                             TRANSFORMED STATE~%")
           (format out "~va~va~va~va   ~va~va~va~va~%" s0w 's e0w 'e c0w 'c d0w 'd s1w 's e1w 'e c1w 'c d1w 'd)
           (hr)
-
           (loop :for rule :in rules 
-             do
+             :do
              (match rule
                ((s0 e0 c0 d0 :_ s1 e1 c1 d1 'where var '= init-form)
                 (format out "~(~va~va~va~va-> ~va~va~va~va~)~%"
@@ -98,36 +101,208 @@
                 (format out "~(~va~va~va~va-> ~va~va~va~va~)~%"
                         s0w s0 e0w e0 c0w c0 d0w d0 s1w s1 e1w e1 c1w c1 d1w d1))))
           (hr))))))
-  
+
+
+;; todo place
+(defun optimizer (program)
+  (cond
+    ((null program) nil)
+    (t
+     (match program
+       ((:AP :RTN)
+        `(:TAPC))
+       (t
+        (cond
+          ((consp program)
+           (cons (optimizer (car program)) (optimizer (cdr program))))
+          (t
+           program)))))))
+
+(defun superinstruction (program)
+  )
+
+(defgeneric run-secd (secd exp)
+  (:documentation "generic launcher."))
+
+;; (defmacro def-secd-machine (name doc &rest rules)
+;;   "define secd machine."
+;;   (let ((s (gensym "S ")) (e (gensym "E ")) (c (gensym "C ")) (d (gensym "D "))
+;;         (secd (gensym "secd"))
+;;         (exp (gensym "exp ")))
+;;     (validate-rules rules)
+;;     `(progn
+;;        (defparameter ,name (describe-secd ',rules))
+;;        (defun ,name (,s ,e ,c ,d)
+;;          ,doc
+;;          (declare (optimize (speed 3) (safety 0)))
+;;          (let ((,secd (list ,s ,e ,c ,d)))
+;;            (tagbody
+;;             :loop
+;;               ;;(let ((*print-circle* t))
+;;               ;;  (format t ";; ~s~%" ,secd))
+;;               (match ,secd
+;;                 ,@(loop :for rule :in rules
+;;                      :collect
+;;                      (match rule
+;;                        ((s0 e0 c0 d0 :_ s1 e1 c1 d1 'where var '= init-form)
+;;                         `((,s0 ,e0 ,c0 ,d0)
+;;                           (let ((,var ,init-form))
+;;                             (psetf (nth 0 ,secd) ,(pattern->cons s1)
+;;                                    (nth 1 ,secd) ,(pattern->cons e1)
+;;                                    (nth 2 ,secd) ,c1
+;;                                    (nth 3 ,secd) ,(pattern->cons d1))
+;;                             (go :loop))))
+;;                        ((s0 e0 c0 d0 :_ s1 e1 c1 d1)
+;;                         `((,s0 ,e0 ,c0 ,d0)
+;;                           (psetf (nth 0 ,secd) ,(pattern->cons s1)
+;;                                  (nth 1 ,secd) ,(pattern->cons e1)
+;;                                  (nth 2 ,secd) ,c1
+;;                                  (nth 3 ,secd) ,(pattern->cons d1))
+;;                           (go :loop)))))))
+;;            ,secd))
+;;        (defmethod run-secd ((secd (eql ,(as-keyword name))) ,exp)
+;;          (let ((,c (compile-pass1 ,exp ())))
+;;            (format t ";; code: ~s~%" ,c)
+;;            (,name ',s ',e ,c ',d)))
+;;        ',name)))
+
+
+;; (defmacro def-secd-machine (name doc &rest rules)
+;;   "define secd machine."
+;;   (let ((s (gensym "S ")) (e (gensym "E ")) (c (gensym "C ")) (d (gensym "D "))
+;;         (exp (gensym "exp ")))
+;;     (validate-rules rules)
+;;     `(progn
+;;        (defparameter ,name (describe-secd ',rules))
+;;        (defun ,name (,s ,e ,c ,d)
+;;          ,doc
+;;          (declare (optimize (speed 3) (safety 0)))
+;;          (tagbody
+;;           :loop
+;;             ;; (let ((*print-circle* t))
+;;             ;;  (format t ";; ~s~%" (list ,s ,e ,c ,d)))
+;;             (match ,c
+;;               ,@(loop :for rule :in rules
+;;                    :collect
+;;                    (match rule
+;;                      ((s0 e0 c0 d0 :_ s1 e1 c1 d1 'where var '= init-form)
+;;                       `(,c0
+;;                         (match (list ,s ,e ,d)
+;;                           ((,s0 ,e0 ,d0)
+;;                            (let ((,var ,init-form))
+;;                              (psetq ,s ,(pattern->cons s1)
+;;                                     ,e ,(pattern->cons e1)
+;;                                     ,c ,c1
+;;                                     ,d ,(pattern->cons d1))
+;;                              (go :loop))))))
+;;                      ((s0 e0 c0 d0 :_ s1 e1 c1 d1)
+;;                       `(,c0
+;;                         (match (list ,s ,e ,d)
+;;                           ((,s0 ,e0 ,d0)
+;;                            (psetq ,s ,(pattern->cons s1)
+;;                                   ,e ,(pattern->cons e1)
+;;                                   ,c ,c1
+;;                                   ,d ,(pattern->cons d1))
+;;                            (go :loop)))))))))
+;;          (list ,s ,e ,c ,d))
+;;        (defmethod run-secd ((secd (eql ,(as-keyword name))) ,exp)
+;;          (let ((,c (compile-pass1 ,exp ())))
+;;            (format t ";; code: ~s~%" ,c)
+;;            (,name ',s ',e ,c ',d)))
+;;        ',name)))
+
+;; no list ver.
 (defmacro def-secd-machine (name doc &rest rules)
   "define secd machine."
-  (let ((s (gensym "S ")) (e (gensym "E ")) (c (gensym "C ")) (d (gensym "D ")))
+  (let ((s (gensym "S ")) (e (gensym "E ")) (c (gensym "C ")) (d (gensym "D "))
+        (exp (gensym "exp ")))
     (validate-rules rules)
     `(progn
        (defparameter ,name (describe-secd ',rules))
        (defun ,name (,s ,e ,c ,d)
          ,doc
+         (declare (optimize (speed 3) (safety 0)))
          (tagbody
           :loop
-            ;;(format t ";; ~s~%" (list ,s ,e ,c ,d))
-            (match (list ,s ,e ,c ,d)
+            ;; (let ((*print-circle* t))
+            ;;  (format t ";; ~s~%" (list ,s ,e ,c ,d)))
+            (match ,c
               ,@(loop :for rule :in rules
                    :collect
                    (match rule
                      ((s0 e0 c0 d0 :_ s1 e1 c1 d1 'where var '= init-form)
-                      `((,s0 ,e0 ,c0 ,d0)
-                        (let ((,var ,init-form))
-                          (psetq ,s ,(pattern->cons s1)
-                                 ,e ,(pattern->cons e1)
-                                 ,c ,c1
-                                 ,d ,(pattern->cons d1))
-                          (go :loop))))
+                      `(,c0
+                        (match ,s
+                          (,s0
+                           (match ,e
+                             (,e0
+                              (match ,d
+                                (,d0
+                                 (let ((,var ,init-form))
+                                   (psetq ,s ,(pattern->cons s1)
+                                          ,e ,(pattern->cons e1)
+                                          ,c ,c1
+                                          ,d ,(pattern->cons d1))
+                                   (go :loop))))))))))
                      ((s0 e0 c0 d0 :_ s1 e1 c1 d1)
-                      `((,s0 ,e0 ,c0 ,d0)
-                        (psetq ,s ,(pattern->cons s1)
-                               ,e ,(pattern->cons e1)
-                               ,c ,c1
-                               ,d ,(pattern->cons d1))
-                        (go :loop)))))))
+                      `(,c0
+                        (match ,s
+                          (,s0
+                           (match ,e
+                             (,e0
+                              (match ,d
+                                (,d0
+                                 (psetq ,s ,(pattern->cons s1)
+                                        ,e ,(pattern->cons e1)
+                                        ,c ,c1
+                                        ,d ,(pattern->cons d1))
+                                 (go :loop)))))))))))))
          (list ,s ,e ,c ,d))
+       (defmethod run-secd ((secd (eql ,(as-keyword name))) ,exp)
+         (let ((,c (compile-pass1 ,exp ())))
+           (format t ";; code: ~s~%" ,c)
+           (,name ',s ',e ,c ',d)))
        ',name)))
+
+;; (defmacro def-secd-machine (name doc &rest rules)
+;;   "define secd machine."
+;;   (let ((s (gensym "S ")) (e (gensym "E ")) (c (gensym "C ")) (d (gensym "D "))
+;;         (exp (gensym "exp ")))
+;;     (validate-rules rules)
+;;     `(progn
+;;        (defparameter ,name (describe-secd ',rules))
+;;        (defun ,name (,s ,e ,c ,d)
+;;          ,doc
+;;          (declare (optimize (speed 3) (safety 0)))
+;;          (tagbody
+;;           :loop
+;;             ;; (let ((*print-circle* t))
+;;             ;;   (format t ";; ~s~%" (list ,s ,e ,c ,d)))
+;;             (match (list ,s ,e ,c ,d)
+;;               ,@(loop :for rule :in rules
+;;                    :collect
+;;                    (match rule
+;;                      ((s0 e0 c0 d0 :_ s1 e1 c1 d1 'where var '= init-form)
+;;                       `((,s0 ,e0 ,c0 ,d0)
+;;                         (let ((,var ,init-form))
+;;                           (psetq ,s ,(pattern->cons s1)
+;;                                  ,e ,(pattern->cons e1)
+;;                                  ,c ,c1
+;;                                  ,d ,(pattern->cons d1))
+;;                           (go :loop))))
+;;                      ((s0 e0 c0 d0 :_ s1 e1 c1 d1)
+;;                       `((,s0 ,e0 ,c0 ,d0)
+;;                         (psetq ,s ,(pattern->cons s1)
+;;                                ,e ,(pattern->cons e1)
+;;                                ,c ,c1
+;;                                ,d ,(pattern->cons d1))
+;;                         (go :loop)))))))
+;;          (list ,s ,e ,c ,d))
+;;        (defmethod run-secd ((secd (eql ,(as-keyword name))) ,exp)
+;;            (let ((,c (compile-pass1 ,exp ())))
+;;              (format t ";; code: ~s~%" ,c)
+;;              (,name ',s ',e ,c ',d)))
+;;        ',name)))
+
+
+
