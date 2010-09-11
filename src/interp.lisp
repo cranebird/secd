317514d8d8f7e9c2321faa7182dabab49eef6c3f
cranebird <quasi@kc4.so-net.ne.jp>
2010-09-11 22:12:08 +0900

    add secd-eval.

--- src/interp.lisp
+++ src/interp.lisp
@@ -113,11 +113,7 @@ rule: left-hand-side -> right-hand-side or left-hand-side -> right-hand-side whe
   (let ((states
          (loop :for rule :in rules
             :collect
-            (match rule
-              ((s0 e0 c0 d0 :_ s1 e1 c1 d1 'where var '= init-form)
-               (list s0 e0 c0 d0 s1 e1 c1 d1))
-              ((s0 e0 c0 d0 :_ s1 e1 c1 d1)
-               (list s0 e0 c0 d0 s1 e1 c1 d1))))))
+            (append (rule-lhs rule) (rule-rhs rule)))))
     (loop :for (s0 e0 c0 d0 s1 e1 c1 d1) :in states
        :with margin = 3
        :maximize (length (mkstr s0)) :into s0w
@@ -133,28 +129,34 @@ rule: left-hand-side -> right-hand-side or left-hand-side -> right-hand-side whe
 (defun describe-secd (rules)
   "Describe secd rules."
   (with-output-to-string (out)
-    (destructuring-bind  (s0w e0w c0w d0w s1w e1w c1w d1w)
-        (max-rule-width rules)
-      (let ((w (+ (apply #'+ (list s0w e0w c0w d0w s1w e1w c1w d1w))
-                  (length "-> "))))
-        (flet ((hr () (loop :repeat w :do (format out "-") :finally (format out "~%"))))
-          (format out
-                  "           INITIAL STATE                                             TRANSFORMED STATE~%")
-          (format out "~va~va~va~va   ~va~va~va~va~%" s0w 's e0w 'e c0w 'c d0w 'd s1w 's e1w 'e c1w 'c d1w 'd)
-          (hr)
-          (loop :for rule :in rules 
-             :do
-             (match rule
-               ((s0 e0 c0 d0 :_ s1 e1 c1 d1 'where var '= init-form)
-                (format out "~(~va~va~va~va-> ~va~va~va~va~)~%"
-                        s0w s0 e0w e0 c0w c0 d0w d0 s1w s1 e1w e1 c1w c1 d1w d1)
-                (format out "~vtwhere ~(~a = ~a~)~%"
-                        (apply #'+ (list s0w e0w c0w d0w (length "-> ")))
-                        var init-form))
-               ((s0 e0 c0 d0 :_ s1 e1 c1 d1)
-                (format out "~(~va~va~va~va-> ~va~va~va~va~)~%"
-                        s0w s0 e0w e0 c0w c0 d0w d0 s1w s1 e1w e1 c1w c1 d1w d1))))
-          (hr))))))
+    (let ((*print-pretty* nil))
+      (destructuring-bind  (s0w e0w c0w d0w s1w e1w c1w d1w)
+          (max-rule-width rules)
+        (let ((w (+ (apply #'+ (list s0w e0w c0w d0w s1w e1w c1w d1w))
+                    (length "-> "))))
+          (flet ((hr () (loop :repeat w :do (format out "-") :finally (format out "~%"))))
+            (format out
+                    "           INITIAL STATE                                                  TRANSFORMED STATE~%")
+            (format out "~va~va~va~va   ~va~va~va~va~%" s0w 's e0w 'e c0w 'c d0w 'd s1w 's e1w 'e c1w 'c d1w 'd)
+            (hr)
+            (loop :for rule :in rules 
+               :do
+               (let ((s0 (nth 0 (rule-lhs rule)))
+                     (e0 (nth 1 (rule-lhs rule)))
+                     (c0 (nth 2 (rule-lhs rule)))
+                     (d0 (nth 3 (rule-lhs rule)))
+                     (s1 (nth 0 (rule-rhs rule)))
+                     (e1 (nth 1 (rule-rhs rule)))
+                     (c1 (nth 2 (rule-rhs rule)))
+                     (d1 (nth 3 (rule-rhs rule))))
+                 (progn
+                   (format out "~(~va~va~va~va-> ~va~va~va~va~)~%"
+                           s0w s0 e0w e0 c0w c0 d0w d0 s1w s1 e1w e1 c1w c1 d1w d1)
+                   (when (rule-init-form rule)
+                     (format out "~vtwhere ~(~a = ~a~)~%"
+                             (apply #'+ (list s0w e0w c0w d0w (length "-> ")))
+                             (rule-var rule) (rule-init-form rule))))))
+            (hr)))))))
 
 (defparameter *secd-debug* nil)
 
@@ -163,16 +165,23 @@ rule: left-hand-side -> right-hand-side or left-hand-side -> right-hand-side whe
   (let ((transitions (cdr (assoc :transitions spec)))
         (last-value (cadr (assoc :last-value spec))))
     (let* ((states (loop :for s :in states :collect (gensym (mkstr s))))
-           (rules (mapcar #'(lambda (tr)
-                              (compile-transition states tr)) transitions)))
-      (loop :for rule :in rules :for i :from 0
-         :do (format t "rule[~s] = ~s~%" i rule))
+           (rules (compile-transitions states transitions))
+           (desc (describe-secd rules)))
+      ;;(loop :for rule :in rules :for i :from 0
+      ;;   :do (format t "rule[~s] = ~s~%" i rule))
       `(progn
-         ;(defparameter ,name ',rules)
+         (format t ";; ~a transition~%~a~%" ',name ,desc)
          (defun ,name (,@states)
+           ,desc
            ;;(declare (optimize (debug 0) (speed 3) (safety 0)))
            (when *secd-debug*
              (format t "STATE:~% S = ~s~% E = ~s~% C = ~s~% D = ~s~%" ,@states))
            ,(rules->match-n rules `(,name ,@states) last-value))
          (export ',name)))))
 
+(defun secd-eval (exp &key (debug nil) (optimize t))
+  "Eval an expression."
+  (let* ((c (opt (compile-pass1 exp))))
+    (let ((*secd-debug* debug)
+          (*print-circle* t))
+      (secd 's0 'e0 `(,@c . c0) 'd0))))
